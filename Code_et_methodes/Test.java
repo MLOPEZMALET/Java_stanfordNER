416d70a17cd848941d1fbe2569c9b8dc9539adb6
MLOPEZMALET <melanie.lopezmalet@gmail.com>
2020-03-26 19:48:00 +0100

    ajout méthodes

---
+++ Code_et_methodes/Test.java
@@ -0,0 +1,21 @@
+package ner_new;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		ReconnaissanceEN a = new ReconnaissanceEN("/home/mlopezmalet/MLOPEZMALET/m2/java/NER_projet/minicorpus");
+		
+		System.out.println("Toutes EN du corpus par fichier: " + a.getAllEN());
+		System.out.println("Liste des Tags du corpus: " + a.getListTag());
+		System.out.println("Liste des EN dans un fichier: " + a.getENInFile("15.txt"));		
+		System.out.println("Liste des EN d'un seul tag dans un fichier: " + a.getENInFileByTag("18.txt", "PERSON"));
+		System.out.println("Textes contenant un tag: " + a.getTexts("PERSON"));
+		System.out.println("Textes contenant une EN correspondant à un tag: " + a.getTextsTag("Alec","PERSON"));
+		System.out.println("EN du corpus correspondant à un tag: " + a.getENByTag("PERSON"));
+	}
+
+}
